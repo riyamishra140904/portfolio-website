d7d314ff25eb389a324004758cc3ed1dcb5008b6
riyamishra140904 <riyamishra140904@gmail.com>
2026-04-29 21:06:42 +0530

    Initial commit

---
+++ todo.js
@@ -0,0 +1,21 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerText = task;
+
+    
+    li.onclick = function () {
+        this.style.textDecoration = "line-through";
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
